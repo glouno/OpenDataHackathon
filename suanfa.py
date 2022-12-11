3b0f79005b89c3baa9edf1c4a2b7fe8e6737f495
yezhan14promax <zhan.ye@edu.ece.fr>
2022-12-11 16:14:01 +0100

    优化一下

--- suanfa.py
+++ suanfa.py
@@ -1,9 +1,15 @@
 
-'''marche=[1,2,3]
 
-article=[1,2,3,4,5]
 
-prix=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]'''
+marche=['auchan','lidl','franprix']
+
+tous_articles=['pomme','lait','sel','riz','tomate']
+
+article=['pomme','lait','sel']
+
+prix=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+
 
 def main(marche,article,prix):
     article_marche=[]
@@ -16,27 +22,166 @@ def main(marche,article,prix):
     #print(article_marche_prix)
 
     posibles=[]
-    for n1 in range(len(marche)):
-        for n2 in range(len(marche)):
-            for n3 in range(len(marche)):
-                for n4 in range(len(marche)):
-                    for n5 in range(len(marche)):
-                        a=[marche[n1]]
-                        a.append(marche[n2])
-                        a.append(marche[n3])
-                        a.append(marche[n4])
-                        a.append(marche[n5])
-                        a=tuple(a)
-                        posibles.append(a)
-                        #print(b)
+    if len(article)==1:
+        for n1 in range(len(marche)):
+            a=[marche[n1]]
+            a=tuple(a)
+            posibles.append(a)
+    
+    if len(article)==2:
+        for n1 in range(len(marche)):
+            for n2 in range(len(marche)):
+                a=[marche[n1]]
+                a.append(marche[n2])
+                a=tuple(a)
+                posibles.append(a)
+    
+    if len(article)==3:
+        for n1 in range(len(marche)):
+            for n2 in range(len(marche)):
+                for n3 in range(len(marche)):
+                            a=[marche[n1]]
+                            a.append(marche[n2])
+                            a.append(marche[n3])
+                            a=tuple(a)
+                            posibles.append(a)
+
+    if len(article)==4:
+        for n1 in range(len(marche)):
+            for n2 in range(len(marche)):
+                for n3 in range(len(marche)):
+                    for n4 in range(len(marche)):
+                            a=[marche[n1]]
+                            a.append(marche[n2])
+                            a.append(marche[n3])
+                            a.append(marche[n4])
+                            a=tuple(a)
+                            posibles.append(a)
+
+    if len(article)==5:
+        for n1 in range(len(marche)):
+            for n2 in range(len(marche)):
+                for n3 in range(len(marche)):
+                    for n4 in range(len(marche)):
+                        for n5 in range(len(marche)):
+                            a=[marche[n1]]
+                            a.append(marche[n2])
+                            a.append(marche[n3])
+                            a.append(marche[n4])
+                            a.append(marche[n5])
+                            a=tuple(a)
+                            posibles.append(a)
+    
+    if len(article)==6:
+        for n1 in range(len(marche)):
+            for n2 in range(len(marche)):
+                for n3 in range(len(marche)):
+                    for n4 in range(len(marche)):
+                        for n5 in range(len(marche)):
+                            for n6 in range(len(marche)):
+                                a=[marche[n1]]
+                                a.append(marche[n2])
+                                a.append(marche[n3])
+                                a.append(marche[n4])
+                                a.append(marche[n5])
+                                a.append(marche[n6])
+                                a=tuple(a)
+                                posibles.append(a)
+    
+    if len(article)==7:
+        for n1 in range(len(marche)):
+            for n2 in range(len(marche)):
+                for n3 in range(len(marche)):
+                    for n4 in range(len(marche)):
+                        for n5 in range(len(marche)):
+                            for n6 in range(len(marche)):
+                                for n7 in range(len(marche)):
+                                    a=[marche[n1]]
+                                    a.append(marche[n2])
+                                    a.append(marche[n3])
+                                    a.append(marche[n4])
+                                    a.append(marche[n5])
+                                    a.append(marche[n6])
+                                    a.append(marche[n7])
+                                    a=tuple(a)
+                                    posibles.append(a)
+    if len(article)==8:
+        for n1 in range(len(marche)):
+            for n2 in range(len(marche)):
+                for n3 in range(len(marche)):
+                    for n4 in range(len(marche)):
+                        for n5 in range(len(marche)):
+                            for n6 in range(len(marche)):
+                                for n7 in range(len(marche)):
+                                    for n8 in range(len(marche)):
+                                        a=[marche[n1]]
+                                        a.append(marche[n2])
+                                        a.append(marche[n3])
+                                        a.append(marche[n4])
+                                        a.append(marche[n5])
+                                        a.append(marche[n6])
+                                        a.append(marche[n7])
+                                        a.append(marche[n8])
+                                        a=tuple(a)
+                                        posibles.append(a)
+    
+    if len(article)==9:
+        for n1 in range(len(marche)):
+            for n2 in range(len(marche)):
+                for n3 in range(len(marche)):
+                    for n4 in range(len(marche)):
+                        for n5 in range(len(marche)):
+                            for n6 in range(len(marche)):
+                                for n7 in range(len(marche)):
+                                    for n8 in range(len(marche)):
+                                        for n9 in range(len(marche)):
+                                            a=[marche[n1]]
+                                            a.append(marche[n2])
+                                            a.append(marche[n3])
+                                            a.append(marche[n4])
+                                            a.append(marche[n5])
+                                            a.append(marche[n6])
+                                            a.append(marche[n7])
+                                            a.append(marche[n8])
+                                            a.append(marche[n9])
+                                            a=tuple(a)
+                                            posibles.append(a)
+    
+    if len(article)==10:
+        for n1 in range(len(marche)):
+            for n2 in range(len(marche)):
+                for n3 in range(len(marche)):
+                    for n4 in range(len(marche)):
+                        for n5 in range(len(marche)):
+                            for n6 in range(len(marche)):
+                                for n7 in range(len(marche)):
+                                    for n8 in range(len(marche)):
+                                        for n9 in range(len(marche)):
+                                            for n10 in range(len(marche)):
+                                                a=[marche[n1]]
+                                                a.append(marche[n2])
+                                                a.append(marche[n3])
+                                                a.append(marche[n4])
+                                                a.append(marche[n5])
+                                                a.append(marche[n6])
+                                                a.append(marche[n7])
+                                                a.append(marche[n8])
+                                                a.append(marche[n9])
+                                                a.append(marche[n10])
+                                                a=tuple(a)
+                                                posibles.append(a)
+
+
+                        
+                        
     tousprixs={}
     for i in posibles:
         n=0
         prixs=0
-        while n<5 :
+        while n<len(article) :
             prixs=prixs+article_marche_prix.get((article[n],i[n]))
             n+=1
-        if n==5:
+        if n==len(article):
             tousprixs[i] = prixs
     #print(tousprixs)
     min_prixs = min(zip(tousprixs.values(),tousprixs.keys()))
@@ -55,8 +200,8 @@ def main(marche,article,prix):
 
 
 if __name__ == '__main__':
-    main()
-    #main(marche,article,prix)
+    #main()
+    main(marche,article,prix)
 
 
 
